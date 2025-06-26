16107a1fbd448905abddc4e80b6e8d659a3fb226
DeKok, Matt G. <matt.dekok@gmail.com>
2025-06-25 21:18:09 -0500

    Update documentation in drizzle.ts and json.ts

--- src/json.ts
+++ src/json.ts
@@ -1,4 +1,5 @@
 /**
+ * @module
  * This module provides a JSON search utility that extends {@linkcode AdvancedSearchParser} to filter arrays of JSON data using advanced search queries.
  *
  * @example
@@ -20,8 +21,6 @@
  * const result = parser.filter('author:Tolkien');
  * // result: [{ title: "The Hobbit", author: "Tolkien" }]
  * ```
- * 
- * @module
  */
 import { AdvancedSearchParser, type ASTNode } from "./index";
 
