0dcfec80e3b8440df22924afca41820fdb297e14
DeKok, Matt G. <matt.dekok@gmail.com>
2025-06-27 11:18:50 -0500

    Fix regex in QueryParser's tokenize method

--- src/index.ts
+++ src/index.ts
@@ -140,7 +140,7 @@ export class QueryParser {
 	// Convert query to tokens
 	private tokenize(query: string): Token[] {
 		const tokens: Token[] = [];
-		const regex = /(?: |^)(-?\()|(\))|(?: |^)(-)|(\w+)(:|=|>=|<=|>|<)(?:(\d{4}-\d{2}-\d{2})|(-?\d+(?:\.\d+)?))|((?:(\w+):)?(?:(\w+)|"([^"]+)"|\/([^\/]+)\/))/g;
+		const regex = /(?: |^)(-?\()|(\))|(?: |^)(-)|(\w+)(:|=|>=|<=|>|<)(?:(\d{4}-\d{2}-\d{2})|(-?\d+(?:\.\d+)?))|(?:(\w+):)?(?:(\w+)|"([^"]+)"|\/([^\/]+)\/)/g;
 		let match: RegExpExecArray | null;
     
 		while (match = regex.exec(query)) {
